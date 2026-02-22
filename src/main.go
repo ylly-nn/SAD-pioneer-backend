fdfcdbe14d204a3c04c7d81e3e33d66f34b08bf0
ylly-nn <ylly_nn@mail.ru>
2026-02-23 00:19:20 +0400

    feat: Добавлен тестовый endpoint

--- src/main.go
+++ src/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
@@ -11,6 +12,14 @@ import (
 func main() {
 	r := mux.NewRouter()
 
+	 // Тестовый эндпоинт
+    r.HandleFunc("/test", testHandler).Methods("GET")
+
 	log.Println("Сервер запущен на http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", r))
+}
+
+func testHandler(w http.ResponseWriter, r *http.Request) {
+    w.WriteHeader(http.StatusOK)
+    fmt.Fprint(w, "OK")
 }
